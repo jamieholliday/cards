0139dfeaa4376d2944bed61649eb8afb6e674b0b
Jamie Holliday <jamie@jamieholliday.co.uk>
2015-01-25 16:11:48 +0000

    Create new games and players in firebase

--- app/main.js
+++ app/main.js
@@ -1,15 +1,27 @@
 'use strict';
 
 angular.module('cards', ['ui.router', 'firebase'])
+.constant('FIREBASE_URL', 'https://popping-inferno-5313.firebaseio.com')
 .config(function($stateProvider, $urlRouterProvider, $locationProvider, $provide) {
 
-	$locationProvider.html5Mode(true);
-	$urlRouterProvider.otherwise('/');
+//	$locationProvider.html5Mode(true);
+	$urlRouterProvider.otherwise('/new');
 
 	$stateProvider
-		.state('home', {
-			url: '/',
-			templateUrl: 'views/home.html',
-			controller: 'homeCtrl as home'
+		.state('newGame', {
+			url: '/new',
+			templateUrl: 'views/newGame.html',
+			controller: 'newGameCtrl as newGame'
 		})
+        .state('joinGame', {
+            url: '/join/:gameId',
+            templateUrl: 'views/joinGame.html',
+            controller: 'joinGameCtrl as joinGame'
+        })
+        .state('currentGame', {
+            url: '/game/:gameId',
+            templateUrl: 'views/currentGame.html',
+            controller: 'currentGameCtrl as currentGame'
+        })
+        
 });
--- app/modules/home/home.controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('cards')
-.controller('homeCtrl', function() {
-    'use strict';
-    var home = this;
-    
-    home.btnText = 'Join'
-});
